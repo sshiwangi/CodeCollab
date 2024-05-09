cbd93394651587be5e4a89604712c77c3e47e0a3
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2024-05-09 08:54:19 +0530

    adding more pages to the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import Calendar from "./pages/Calender";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
+import Settings from "./pages/Settings";
 import Signup from "./pages/Signup";
 import "./styles/tailwind.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -14,6 +16,8 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
+         <Route exact path="/settings" element={<Settings />} />
+         <Route exact path="/calender" element={<Calendar />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/SidebarDrawer.jsx
+++ frontend/src/components/SidebarDrawer.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function SidebarDrawer({ isOpen, closeDrawer }) {
   return (
-     <div className={` text-white bg-blue-500 h-screen font-bold flex flex-col drawer w-1/4 ${isOpen ? 'drawer-open bg-blue-500' : ''}`}>
-      <div className='p-4'>Logo</div>
-      <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay" onClick={closeDrawer}></label>
+    <div
+      className={` text-white bg-blue-500 h-screen font-bold flex flex-col drawer w-1/4 ${
+        isOpen ? "drawer-open bg-blue-500" : ""
+      }`}
+    >
+      <div className="p-4">Logo</div>
+      <label
+        htmlFor="my-drawer"
+        aria-label="close sidebar"
+        className="drawer-overlay"
+        onClick={closeDrawer}
+      ></label>
       <ul className="menu text-white p-4 w-full min-h-full bg-blue-500">
         {/* Sidebar content here */}
-        <li><a>Dashboard</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Discussions</a></li>
+        <li>
+          <a>Dashboard</a>
+        </li>
+        <li>
+          <a>Projects</a>
+        </li>
+        <li>
+          <a>Discussions</a>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default SidebarDrawer
+export default SidebarDrawer;
